9e358d1a15df5716d7fa08fdb5f92e77b7317215
Xanuthatusu <pategeorge12@gmail.com>
2016-04-29 14:14:03 -0600

    update to make the javascript inject css and type

--- js/script.js
+++ js/script.js
@@ -1,18 +1,37 @@
-var showText = function (target, message, index, interval) {
-	if (index < message.length) {
-		$(target).append($(message).text()[index++]);
-		setTimeout(function () { showText(target, message, index, interval); }, interval);
-	};
-};
+caption = `
+/*
+ * This is a project of self-generating webpages.
+ * Luckily for you, I'm self aware and will design myself.
+ * Just sit back, relax, and enjoy.
+ */
 
-$(document).ready(function() {
-	$(this).click(function () {
-		showText("#click", "#msg", 0, 100);
-		$("h1").text($("#msg").text()[2]);
-	});
-	jQuery.get("data.txt", function(data) {
-		alert(data);
-		$("h1").text(data.replace("n", ""));
-	});
+body {
+	color: red;
+}
+
+#main {
+	font-family: sans-serif;
+	text-align: center;
+	font-size: 20px;
+}
+`;
+var captionLength = 0;
+
+$(document).ready(function(){
+	type = function type() {
+		$("#main").text(caption.substr(0, captionLength++));
+		if (captionLength < caption.length + 1) {
+			setTimeout('type()', 25);
+		} else {
+			captionLength = 0;
+		}
+	}
+	type();
+	console.log(document.styleSheets[0]);
+	addStyles = function addStyles(rule) {
+		document.styleSheets[0].insertRule(rule, 1);
+	}
+	setTimeout('addStyles("body {color: red;}")', 4500);
+	setTimeout('addStyles("#main {font-family: sans-serif;}")', 5500);
+	setTimeout('addStyles("#main {margin: 0 auto;}")', 6000);
 });
-
